6aee77371c7509c74a9e062eab4a81ccf85a940b
Kareen Ghann <kareenghann@live.com>
2024-01-29 18:22:57 +0000

    new repo

---
+++ Classwork/JavaScript/exercise.js
@@ -0,0 +1,20 @@
+// const name = "Kareen";
+// const age = 31;
+// const location = "East London";
+
+// const quickAboutMe = `My name is ${name}. I am ${age} years old and I currently live in ${location}`;
+
+// console.log(quickAboutMe);
+
+const numOne = 7;
+const numTwo = 3;
+
+const sum = numOne + numTwo;
+const difference = numOne - numTwo;
+const product = numOne * numTwo;
+const quotient = numOne / numTwo;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
---
+++ Classwork/JavaScript/script.js
@@ -0,0 +1,41 @@
+// console.log("Hello World");
+
+const pi = 3.14;
+
+let firstName, lastName;
+let age = 30;
+const country = "United Kingdom";
+
+firstName = "Kareen";
+lastName = "Ghann";
+
+let isStudent = true;
+
+let person = {
+  name: "Kareen",
+  age: 31,
+  isStudent,
+};
+
+console.log("Name is", firstName, lastName);
+console.log("Age is", age);
+console.log("Country I am from is", country);
+
+console.log(person.isStudent);
+
+let a = 10;
+let b = 3;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+let exponentiation = a ** b;
+
+console.log("Sum", sum);
+console.log("Difference", difference);
+console.log("Product", product);
+console.log("Quotient", quotient);
+console.log("Remainder", remainder);
+console.log("Exponentiation", exponentiation);
